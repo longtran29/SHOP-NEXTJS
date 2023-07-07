a3e863d92289e45c91d5541559fbd27a0820e54e
long2904 <tran50787@gmail.com>
2023-07-07 14:54:20 +0700

    feat category

--- pages/api/categories/[categoryId].js
+++ pages/api/categories/[categoryId].js
@@ -7,48 +7,62 @@ async function categories(req, res) {
   const { categoryId } = req.query;
 
   if (req.method === "DELETE") {
-    console.log("Da vao delete");
-    await fetch(`${API_URL}/categories/${categoryId}`, {
+    const resDel = await fetch(`${API_URL}/categories/${categoryId}`, {
       method: "DELETE",
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    })
-      .then((res) => res.json())
-      .then((data) => res.status(200).json({ categories: data }))
-      .catch((err) => res.status(500).json({ message: err }));
-  } else if (req.method === "PUT") {
-    console.log("Value status " + req.body.status);
+    });
 
+    const dataDel = await resDel.json();
+
+    if (!resDel.ok) {
+      res.status(500).json({ message: dataDel.message });
+    } else {
+      res.status(200).json({ categories: dataDel });
+    }
+  } else if (req.method === "PUT") {
     if (req.body.name) {
       // update object existing category
 
-      await fetch(`${API_URL}/categories/${categoryId}`, {
+      const resPut = await fetch(`${API_URL}/categories/${categoryId}`, {
         method: "PUT",
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify(req.body),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log("Data return update " + JSON.stringify(data));
-          res.status(200).json({ categories: data });
-        });
+      });
+
+      const dataPut = await resPut.json();
+
+      if (!resPut.ok) {
+        res.status(500).json({ message: dataPut.message });
+      } else {
+        res.status(200).json({ categories: dataPut });
+      }
     } else {
       // update status
 
-      await fetch(`${API_URL}/categories/status/${categoryId}`, {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-        body: !req.body.status,
-      })
-        .then((res) => res.json())
-        .then((data) => res.status(200).json({ categories: data }));
+      const resPut2 = await fetch(
+        `${API_URL}/categories/status/${categoryId}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: !req.body.status,
+        }
+      );
+
+      const dataPut2 = await resPut2.json();
+
+      if (!resPut2.ok) {
+        res.status(500).json({ message: dataPut2.message });
+      } else {
+        res.status(200).json({ categories: dataPut2 });
+      }
     }
   }
 }
