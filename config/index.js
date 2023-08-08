bf1aef0381c5a55f4046e4619bfbacc0d838c1a7
Trần Quang Long <77282670+longtran29@users.noreply.github.com>
2023-08-08 22:49:44 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL  || 'http://localhost:8080/api/v1'
+export const API_URL = "https://api-laptop-production.up.railway.app/api/v1"
 
 export const NEXT_API =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
+  "https://laptop-store-longtran.vercel.app"
