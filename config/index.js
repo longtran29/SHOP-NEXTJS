9dd82f0270b461a21cd8598cc2a7f8af6f400d58
Trần Quang Long <77282670+longtran29@users.noreply.github.com>
2023-08-08 21:45:40 +0700

    api path enviroment variable

--- config/index.js
+++ config/index.js
@@ -1,9 +1,4 @@
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL  || 'http://localhost:8080/api/v1'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL  || 'http://localhost:8080/api/v1'
 
-// export const NEXT_API =
-//   process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
-
-
-export const API_URL = "https://api-laptop-production.up.railway.app/api/v1"
-
-export const NEXT_API ="https://shop-nextjs-2o6n-htfc894qo-longtran29.vercel.app"
+export const NEXT_API =
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
