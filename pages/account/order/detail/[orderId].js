61b27dbe14eb3584df2c9d18980d02b0f719442b
Trần Quang Long <77282670+longtran29@users.noreply.github.com>
2023-08-09 09:40:59 +0700

    Update [orderId].js

--- pages/account/order/detail/[orderId].js
+++ pages/account/order/detail/[orderId].js
@@ -1,9 +1,9 @@
-import OrderTracker from "@/components/Order/OrderTracker";
-import CustomerLayout from "@/layouts/CustomerLayout";
+// import OrderTracker from "@/components/Order/OrderTracker";
+// import CustomerLayout from "@/layouts/CustomerLayout";
 import React from "react";
 import { API_URL, NEXT_API } from "@/config";
-import OrderAddressCard from "@/components/AddressCard/OrderAddressCard";
-import { Box, Button, Grid } from "@mui/material";
+// import OrderAddressCard from "@/components/AddressCard/OrderAddressCard";
+// import { Box, Button, Grid } from "@mui/material";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 
@@ -42,70 +42,70 @@ function OrderDetails({ orders }) {
 
   return (
     <div className="p-8">
-      <div>
-        <h1 className="font-semibold text-xl">Delivery address</h1>
-        <OrderAddressCard data={orders.address} />
-      </div>
+      // <div>
+      //   <h1 className="font-semibold text-xl">Delivery address</h1>
+      //   <OrderAddressCard data={orders.address} />
+      // </div>
 
-      <div className="py-20">
-        <Grid container className="flex items-center">
-          <Grid item xs={10}>
-            <OrderTracker activeStep={[steps[orders.orderStatus]]} />
-          </Grid>
-          <Grid xs={2} item className="">
-            <button
-              className="bg-primary-400 text-white px-3 py-2 rounded-sm hover:bg-primary-600"
-              onClick={cancelOrder}
-            >
-              Cancel order
-            </button>
-          </Grid>
-        </Grid>
-      </div>
-      <Grid container>
-        {orders.orderDetails.map((detail, index) => (
-          <Grid
-            item
-            container
-            className="shadow-sm border p-4 mb-4 hover:shadow-md hover:shadow-gray-400"
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-            key={index}
-          >
-            <Grid item xs={6} className="flex">
-              <div className="h-[5rem] w-[5rem] hover:cursor-pointer">
-                <img src={detail.product.primaryImage} alt="" />
-              </div>
-              <div className="space-y-2 ml-5">
-                <p
-                  className="hover:cursor-pointer font-semibold"
-                  onClick={() =>
-                    router.push(`/product/detail/${detail.product.id}`)
-                  }
-                >
-                  {detail.product.name.toUpperCase()}
-                </p>
-                <p className="opacity-50 text-sm font-semibold">
-                  Seller: {detail.product.brand.name}
-                </p>
-                <p className="opacity-50 text-sm font-semibold">
-                  Categories: {detail.product.category.name}
-                </p>
-                <p className="font-extralight">
-                  {" "}
-                  $ {detail.product.original_price}
-                </p>
-              </div>
-            </Grid>
-            <Grid item>
-              <Box >
+      // <div className="py-20">
+      //   <Grid container className="flex items-center">
+      //     <Grid item xs={10}>
+      //       <OrderTracker activeStep={[steps[orders.orderStatus]]} />
+      //     </Grid>
+      //     <Grid xs={2} item className="">
+      //       <button
+      //         className="bg-primary-400 text-white px-3 py-2 rounded-sm hover:bg-primary-600"
+      //         onClick={cancelOrder}
+      //       >
+      //         Cancel order
+      //       </button>
+      //     </Grid>
+      //   </Grid>
+      // </div>
+      // <Grid container>
+      //   {orders.orderDetails.map((detail, index) => (
+      //     <Grid
+      //       item
+      //       container
+      //       className="shadow-sm border p-4 mb-4 hover:shadow-md hover:shadow-gray-400"
+      //       display="flex"
+      //       alignItems="center"
+      //       justifyContent="space-between"
+      //       key={index}
+      //     >
+      //       <Grid item xs={6} className="flex">
+      //         <div className="h-[5rem] w-[5rem] hover:cursor-pointer">
+      //           <img src={detail.product.primaryImage} alt="" />
+      //         </div>
+      //         <div className="space-y-2 ml-5">
+      //           <p
+      //             className="hover:cursor-pointer font-semibold"
+      //             onClick={() =>
+      //               router.push(`/product/detail/${detail.product.id}`)
+      //             }
+      //           >
+      //             {detail.product.name.toUpperCase()}
+      //           </p>
+      //           <p className="opacity-50 text-sm font-semibold">
+      //             Seller: {detail.product.brand.name}
+      //           </p>
+      //           <p className="opacity-50 text-sm font-semibold">
+      //             Categories: {detail.product.category.name}
+      //           </p>
+      //           <p className="font-extralight">
+      //             {" "}
+      //             $ {detail.product.original_price}
+      //           </p>
+      //         </div>
+      //       </Grid>
+      //       <Grid item>
+      //         <Box >
               
-              </Box>
-            </Grid>
-          </Grid>
-        ))}
-      </Grid>
+      //         </Box>
+      //       </Grid>
+      //     </Grid>
+      //   ))}
+      // </Grid>
     </div>
   );
 }
